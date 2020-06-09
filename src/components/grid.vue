74041565bdc094ea0cb92a4aacfaff37efca75bf
Stralcops <antoine.miloux@devinfluence.fr>
2020-06-09 18:13:28 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -1,28 +1,30 @@
 <template>
   <!-- // table-header-group; -->
   <div>
- 
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2">The table header</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>The table body</td>
+          <td>with two columns</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Grid",
-  props: {
-    
+  props: {},
+  data: function () {
+    return {};
   },
-
-  
-  data: function() {
-    return {
-     
-    };
-  },
-  mounted() {
-   
-  },
-  methods: {
-  },
+  mounted() {},
+  methods: {},
 };
 </script>
