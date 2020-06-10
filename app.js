b445be62439295c7d0974001f0bf6552d16d0e8e
DevinfluenceBaptisteM <baptiste.mabille@devinfluence.fr>
2020-06-10 10:45:42 +0200

    Affichage du resultat dans la console

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Baptiste:azerty@data-3laxu.gcp.mongodb.net/DATA?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-    const collection = client.db("sample_supplies").collection("sales");
-    console.log(collection);
-    // Faire les RQT ici
+    const collection = client.db("sample_training").collection("tweets");
+    collection.findOne({}, function (err, result) {
+        console.log(result)
+    });
     client.close();
 });
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index.html');
 });
 app.listen(8070, '127.0.0.1')
