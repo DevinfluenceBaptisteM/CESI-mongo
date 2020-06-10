d8a15dfcaf43340f77cc29a173340f40af50440a
Stralcops <antoine.miloux@devinfluence.fr>
2020-06-10 11:01:58 +0200

    sort de jean

--- app.js
+++ app.js
@@ -14,3 +14,25 @@ app.get('/', function(req, res) {
     res.render('index.html');
 });
 app.listen(8070, '127.0.0.1')
+const uri = "mongodb+srv://jeanM:iWjYfAp7COoLJS2Q@cesitest-xb3jc.gcp.mongodb.net/sample_training?retryWrites=true&w=majority";
+const MongoClient = require('mongodb').MongoClient
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    if(err) {
+        return err
+    }
+
+    const collection = client.db("sample_supplies").collection('sales')
+
+
+    //Recherche par customer
+    let gender = "M"
+    
+    collection.find({"customer.gender": "M"}).toArray(function(err, result) {
+        if (err) throw err;
+        result.forEach(sale => {
+            console.log(sale)
+        });
+        client.close();
+    });
+});
