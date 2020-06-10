a594d0e1468e2d9c486a37c45c44002674add1fb
Akim <akim.soula@linkt.fr>
2020-06-10 09:54:01 +0200

    Connection to MONGO

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 app.engine('html', require('ejs').renderFile);
-
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Baptiste:azerty@data-3laxu.gcp.mongodb.net/DATA?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("sample_supplies").collection("sales");
+    console.log(collection);
+    // Faire les RQT ici
+    client.close();
+});
 app.get('/', function(req, res) {
     res.render('index.html');
 });
-
 app.listen(8070, '127.0.0.1')
